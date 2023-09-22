24da0caa06ae8e2f5cc1f904bc9a10362af743c4
unknown <cahyotriatmojo906@gmail.com>
2023-09-23 06:09:08 +0700

    submesion dicoding

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 9000,
+    host: process.env.HOST || 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
